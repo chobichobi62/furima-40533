49ff01d09f942d6e7af85d50484a1e11df1f21fa
chobichobi62 <ono.t@ymail.ne.jp>
2024-04-17 21:58:44 +0900

    カテゴリー選択のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,23 @@ class Item < ApplicationRecord
   #active_storageアソシエーション
   has_one_attached :image
 
+  #空の投稿は保存できないようにする
+  validates :user_id
+  validates :image
+  validates :item_name
+  validates :description
+  validates :category_id
+  validates :item_condition_id
+  validates :delivery_price_id
+  validates :region_id
+  validates :delivery_time_id
+  validates :price
+
+#ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id,numericality: { other_than: 1, message: "can't be blank"} 
+  validates :item_condition_id,numericality: { other_than: 1, message: "can't be blank"} 
+  validates :delivery_price_id,numericality: { other_than: 1, message: "can't be blank"} 
+  validates :region_id,numericality: { other_than: 1, message: "can't be blank"} 
+  validates :delivery_time_id,numericality: { other_than: 1, message: "can't be blank"} 
+  
 end
