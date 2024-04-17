fc6845b9a878a70df53cd3a5b563a2ea54f7b9aa
chobichobi62 <ono.t@ymail.ne.jp>
2024-04-17 15:41:58 +0900

    Rubocopの実行3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -111,7 +111,7 @@ RSpec.describe User, type: :model do
       it 'family_nameが空では登録できない' do
         @user.family_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name is invalid")
+        expect(@user.errors.full_messages).to include('Family name is invalid')
       end
 
       it 'family_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない' do
@@ -123,7 +123,7 @@ RSpec.describe User, type: :model do
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
 
       it 'first_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない' do
@@ -135,7 +135,7 @@ RSpec.describe User, type: :model do
       it 'family_name_kanaが空では登録できない' do
         @user.family_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name kana is invalid")
+        expect(@user.errors.full_messages).to include('Family name kana is invalid')
       end
 
       it 'family_name_kanaのフリガナが全角(カタカナ)でないと登録できない' do
@@ -147,7 +147,7 @@ RSpec.describe User, type: :model do
       it 'first_name_kanaが空では登録できない' do
         @user.first_name_kana = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana is invalid")
+        expect(@user.errors.full_messages).to include('First name kana is invalid')
       end
 
       it 'first_name_kanaのフリガナが全角(カタカナ)でないと登録できない' do
