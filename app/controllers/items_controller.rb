7b63f2fe6c0d83dcf116ac76b1552e3ca1df659f
chobichobi62 <ono.t@ymail.ne.jp>
2024-04-17 21:55:59 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :item_condition_id, :delivery_price_id, :region_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
 end
--- db/migrate/20240417082440_create_items.rb
+++ db/migrate/20240417082440_create_items.rb
@@ -5,7 +5,7 @@ t.references :user,                   null: false, foreign_key: true
 t.string :item_name,                  null: false
 t.text :description,                  null: false
 t.integer :category_id,                null: false
-t.integer :item_condition_idinteger,   null: false
+t.integer :item_condition_id,   null: false
 t.integer :delivery_price_id,          null: false
 t.integer :region_id,                  null: false
 t.integer :delivery_time_id,           null: false
