4a83eac0da2e52d23d5317cb6b5f076469d71417
chobichobi62 <ono.t@ymail.ne.jp>
2024-04-17 21:56:12 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+
+  #アクティブハッシュとのアソシエーション
+  belongs_to :category
+  belongs_to :region
+  belongs_to :item_condition
+  belongs_to :delivery_price
+  belongs_to :delivery_time
+
+  #active_storageアソシエーション
+  has_one_attached :image
+
 end
