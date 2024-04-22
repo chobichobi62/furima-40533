2e5501f43809507956f2a694a037a83652906007
chobichobi62 <ono.t@ymail.ne.jp>
2024-04-22 18:20:27 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :show, :create,:index]
+  resources :items, only: [:new, :show, :create,:index, :edit, :destroy]
   end
